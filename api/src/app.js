d3af467adc942a1815d88b7ed8a1f5ce5fa4571c
Denis <woxer77@gmail.com>
2021-12-11 20:53:51 +0200

    feat: added express, eslint and prettier

---
+++ api/src/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+app.get("/", function (req, res) {
+    res.send("The connection to the 127.0.0.1:" + process.env.APP_PORT + " was successfully created");
+});
+
+app.get("/get", function (req, res) {
+    res.send("An example of a get request when accessing a page /get");
+});
+// eslint-disable-next-line no-undef
+app.listen(process.env.APP_PORT);
+
+var test = "test"; // for eslint
