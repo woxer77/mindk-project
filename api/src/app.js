961569ec7efcbb89a98b811228fb4033488af250
Denis <woxer77@gmail.com>
2021-12-19 17:48:16 +0200

    feat: added docker-compose

--- api/src/app.js
+++ api/src/app.js
@@ -1,12 +1,12 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 
-app.get("/", function (req, res) {
-    res.send("The connection to the 127.0.0.1:" + process.env.APP_PORT + " was successfully created");
+app.get('/', function (req, res) {
+    res.send('The connection to the 127.0.0.1:' + process.env.APP_PORT + ' was successfully created');
 });
 
-app.get("/get", function (req, res) {
-    res.send("An example of a get request when accessing a page /get");
+app.get('/get', function (req, res) {
+    res.send('An example of a get request when accessing a page /get');
 });
 
 app.listen(process.env.APP_PORT);
