52b907662a37111b7ceb27a9c318706557905ee8
Denis <woxer77@gmail.com>
2022-02-23 17:37:38 +0200

    feat: added context

--- front/src/App.js
+++ front/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   BrowserRouter,
   Routes,
@@ -19,124 +19,137 @@ import AddPostContainer from './containers/posts/addPost';
 import EditPostContainer from './containers/posts/editPost';
 import EditUserContainer from './containers/users/editUser';
 import AuthContainer from './containers/auth/auth';
+import authContext from './contexts/authContext';
 
 const queryClient = new QueryClient();
 
 function App() {
+  const [userData, setUserData] = useState({
+    authenticated: true,
+    user: {
+      userId: 1,
+      firstName: 'default name',
+      email: 'default@default.com',
+    },
+    setUserData: () => {},
+  });
+
   return (
     <div className="App">
       <QueryClientProvider client={queryClient}>
-        <ErrorBoundary>
-          <BrowserRouter>
-            <Routes>
-              <Route path="/" element={<p>Hello, this is main page</p>} />
-              <Route path="*" element={<div>Error 404</div>} />
-              <Route path="/date/:date" element={<DateCont />} />
-              <Route path="/posts" element={<PostsContainer />} />
-              <Route path="/posts/add-post" element={<AddPostContainer />} />
-              <Route path="/posts/:id/edit-post" element={<EditPostContainer />} />
-              <Route path="/users" element={<UsersContainer />} />
-              <Route path="/users/:id" element={<UserContainer />} />
-              <Route path="/users/:id/edit-user" element={<EditUserContainer />} />
-              <Route path="/auth" element={<AuthContainer />} />
+        <authContext.Provider value={userData}>
+          <ErrorBoundary>
+            <BrowserRouter>
+              <Routes>
+                <Route path="/" element={<p>Hello, this is main page</p>} />
+                <Route path="*" element={<div>Error 404</div>} />
+                <Route path="/date/:date" element={<DateCont />} />
+                <Route path="/posts" element={<PostsContainer />} />
+                <Route path="/posts/add-post" element={<AddPostContainer />} />
+                <Route path="/posts/:id/edit-post" element={<EditPostContainer />} />
+                <Route path="/users" element={<UsersContainer />} />
+                <Route path="/users/:id" element={<UserContainer />} />
+                <Route path="/users/:id/edit-user" element={<EditUserContainer />} />
+                <Route path="/auth" element={<AuthContainer />} />
 
-              <Route
-                path="/article/:id"
-                element={(
-                  <Article
-                    text="React (also known as React.js or ReactJS) is a free and open-source front-end JavaScript library for building user interfaces based on UI components. It is maintained by Meta (formerly Facebook) and a community of individual developers and companies. React can be used as a base in the development of single-page or mobile applications. However, React is only concerned with state management and rendering that state to the DOM, so creating React applications usually requires the use of additional libraries for routing, as well as certain client-side functionality."
-                    creationDate="07.01.2022"
-                  />
-                )}
-              />
-              <Route
-                path="/profile"
-                element={(
-                  <Profile
-                    fullName="Ohrimenko Denis Yurievich"
-                    birthDate="20.06.2002"
-                    placeOfBirth="Ukraine, Sumy"
-                    educationPlace="Sumy State University"
-                    user={{
-                      name: 'test',
-                      age: 23,
-                      avatar: {
-                        file: {
-                          id: 1,
-                          name: '123.jpg',
-                          path: '/files/1.jpg',
-                        },
-                      },
-                      files: [
-                        {
-                          id: 1,
-                          name: '123.jpg',
-                          path: '/files/1.jpg',
-                        },
-                        {
-                          id: 1,
-                          name: '123.jpg',
-                          path: '/files/1.jpg',
-                        }],
-                      addrr: {
-                        main: {
-                          line1: 'test',
-                          line2: 'test',
-                          city: 'test',
-                          zip: 1234,
-                        },
-                        alt: {
-                          line1: 'test',
-                          line2: 'test',
-                          city: 'test',
-                          zip: 1234,
-                        },
-                      },
-                      friends: [
-                        {
-                          name: 'test',
-                          age: '23',
-                          avatar: {
-                            file: {
-                              id: 1,
-                              name: '123.jpg',
-                              path: '/files/1.jpg',
-                            },
-                          },
-                          files: [
-                            {
-                              id: 1,
-                              name: '123.jpg',
-                              path: '/files/1.jpg',
-                            },
-                            {
-                              id: 1,
-                              name: '123.jpg',
-                              path: '/files/1.jpg',
-                            }],
-                          addrr: {
-                            main: {
-                              line1: 'test',
-                              line2: 'test',
-                              city: 'test',
-                              zip: 1234,
-                            },
-                            alt: {
-                              line1: 'test',
-                              line2: 'test',
-                              city: 'test',
-                              zip: 1234,
-                            },
+                <Route
+                  path="/article/:id"
+                  element={(
+                    <Article
+                      text="React (also known as React.js or ReactJS) is a free and open-source front-end JavaScript library for building user interfaces based on UI components. It is maintained by Meta (formerly Facebook) and a community of individual developers and companies. React can be used as a base in the development of single-page or mobile applications. However, React is only concerned with state management and rendering that state to the DOM, so creating React applications usually requires the use of additional libraries for routing, as well as certain client-side functionality."
+                      creationDate="07.01.2022"
+                    />
+                  )}
+                />
+                <Route
+                  path="/profile"
+                  element={(
+                    <Profile
+                      fullName="Ohrimenko Denis Yurievich"
+                      birthDate="20.06.2002"
+                      placeOfBirth="Ukraine, Sumy"
+                      educationPlace="Sumy State University"
+                      user={{
+                        name: 'test',
+                        age: 23,
+                        avatar: {
+                          file: {
+                            id: 1,
+                            name: '123.jpg',
+                            path: '/files/1.jpg',
                           },
                         },
-                      ],
-                    }}
-                  />
-                )}
-              />
-            </Routes>
-          </BrowserRouter>
-        </ErrorBoundary>
+                        files: [
+                          {
+                            id: 1,
+                            name: '123.jpg',
+                            path: '/files/1.jpg',
+                          },
+                          {
+                            id: 1,
+                            name: '123.jpg',
+                            path: '/files/1.jpg',
+                          }],
+                        addrr: {
+                          main: {
+                            line1: 'test',
+                            line2: 'test',
+                            city: 'test',
+                            zip: 1234,
+                          },
+                          alt: {
+                            line1: 'test',
+                            line2: 'test',
+                            city: 'test',
+                            zip: 1234,
+                          },
+                        },
+                        friends: [
+                          {
+                            name: 'test',
+                            age: '23',
+                            avatar: {
+                              file: {
+                                id: 1,
+                                name: '123.jpg',
+                                path: '/files/1.jpg',
+                              },
+                            },
+                            files: [
+                              {
+                                id: 1,
+                                name: '123.jpg',
+                                path: '/files/1.jpg',
+                              },
+                              {
+                                id: 1,
+                                name: '123.jpg',
+                                path: '/files/1.jpg',
+                              }],
+                            addrr: {
+                              main: {
+                                line1: 'test',
+                                line2: 'test',
+                                city: 'test',
+                                zip: 1234,
+                              },
+                              alt: {
+                                line1: 'test',
+                                line2: 'test',
+                                city: 'test',
+                                zip: 1234,
+                              },
+                            },
+                          },
+                        ],
+                      }}
+                    />
+                  )}
+                />
+              </Routes>
+            </BrowserRouter>
+          </ErrorBoundary>
+        </authContext.Provider>
       </QueryClientProvider>
     </div>
   );
---
+++ front/src/contexts/authContext.js
@@ -0,0 +1,7 @@
+import { createContext } from 'react';
+
+const authContext = createContext({
+  authenticated: false,
+});
+
+export default authContext;
